65e2323f00a4e1bad27e4a240641b2b8ad6cca82
Travis Van Couvering <travis@tvanc.com>
2021-06-23 19:46:06 -0700

    Add shared methods to PolygonInterface

--- src/gpc/geometry/PolygonInterface.ts
+++ src/gpc/geometry/PolygonInterface.ts
@@ -2,4 +2,32 @@ import ArrayList from "../util/ArrayList";
 
 export default interface PolygonInterface {
   m_List: ArrayList
+
+  equals
+  hashCode
+  toString
+  clear
+  add
+  addPointXY
+  addPoint
+  addPoly
+  isEmpty
+  getBounds
+  getInnerPoly
+  getNumInnerPoly
+  getNumPoints
+  getX
+  getY
+  getPoint
+  getPoints
+  isPointInside
+  isHole
+  setIsHole
+  isContributing
+  setContributing
+  intersection
+  union
+  xor
+  difference
+  getArea
 }
