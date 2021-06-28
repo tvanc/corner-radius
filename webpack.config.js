6c50159791b8f43c1bd274849da74ffadcb9f0ba
Travis Van Couvering <travis@tvanc.com>
2021-06-27 20:57:07 -0700

    Install SCSS

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,7 @@ module.exports = {
       { test: /\.tsx?$/, loader: "ts-loader" },
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
       { test: /\.js$/, loader: "source-map-loader" },
+      { test: /\.scss$/, loader: "sass-loader" },
     ],
   },
 }
