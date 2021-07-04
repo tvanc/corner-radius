588097d99dc2e110fea41abe54fc274d71164c91
Travis Van Couvering <travis@tvanc.com>
2021-07-03 20:02:50 -0700

    Use webpack-encore-powered webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,43 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin")
-const path = require("path")
-// const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const Encore = require("@symfony/webpack-encore")
 
-module.exports = {
-  entry: {
-    main: {
-      import: "./src/index.ts",
-    },
-    menuExample: {
-      import: "./example/menu/index.ts",
-    },
-    gpcExample: {
-      import: "./example/gpc/index.ts",
-    },
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "dist/[name].js",
-  },
-  // Enable sourcemaps for debugging webpack's output.
-  devtool: "inline-source-map",
-  resolve: {
-    // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: ["", ".ts", ".js"],
-  },
-  module: {
-    // TS rules must come before JS
-    rules: [
-      // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
-      { test: /\.tsx?$/, loader: "ts-loader" },
-      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-      { test: /\.js$/, loader: "source-map-loader" },
-      {
-        test: /\.s?css$/,
-        use: [
-          // { loader: MiniCssExtractPlugin.loader },
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
-        ],
-      },
-    ],
-  },
+// Manually configure the runtime environment if not already configured yet by the "encore" command.
+// It's useful when you use tools that rely on webpack.config.js file.
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+  Encore.configureRuntimeEnvironment(process.env.NODE_ENV || "dev")
 }
+
+Encore
+  // directory where compiled assets will be stored
+  .setOutputPath("dist/")
+  // public path used by the web server to access the output path
+  .setPublicPath("/dist")
+  .addEntry("main", "./src/index.ts")
+  .addEntry("menuExample", "example/menu/index.ts")
+  .addEntry("gpcExample", "example/gpc/index.ts")
+  .splitEntryChunks()
+
+  // will require an extra script tag for runtime.js
+  // but, you probably want this, unless you're building a single-page app
+  .enableSingleRuntimeChunk()
+
+  // region Features
+  .cleanupOutputBeforeBuild()
+  .enableBuildNotifications()
+  .enableSourceMaps(!Encore.isProduction())
+  // enables hashed filenames (e.g. app.abc123.css)
+  .enableVersioning(Encore.isProduction())
+
+  // enables @babel/preset-env polyfills
+  .configureBabelPresetEnv((config) => {
+    config.useBuiltIns = "usage"
+    config.corejs = 3
+  })
+
+  // enables Sass/SCSS support
+  .enableSassLoader()
+
+  // uncomment if you use TypeScript
+  .enableTypeScriptLoader()
+// endregion features
+
+module.exports = Encore.getWebpackConfig()
