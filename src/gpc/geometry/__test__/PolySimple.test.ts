be7858e8fd18a16a5a7939c03c567fbfce585022
Travis Van Couvering <travis@tvanc.com>
2021-07-21 21:44:37 -0700

    Simplify test

--- src/gpc/geometry/__test__/PolySimple.test.ts
+++ src/gpc/geometry/__test__/PolySimple.test.ts
@@ -1,25 +1,24 @@
 import PolySimple from "../PolySimple"
 import Point from "../Point"
 
-it("Removes unnecessary points", () => {
+it("Removes unnecessary points from square", () => {
   const poly = new PolySimple()
-
   poly.add([
-    new Point(1, 1),
-    new Point(2, 1),
+    new Point(0, 0),
+    new Point(1, 0), // | - on the same line
+    new Point(2, 0),
+    new Point(2, 1), // could be eliminated
     new Point(2, 2),
-    new Point(3, 2),
-    new Point(3, 1),
-    new Point(1, 1),
+    new Point(1, 2), // could be eliminated
+    new Point(0, 1),
   ])
 
   poly.removeUnnecessaryPoints()
 
   expect(poly.getPoints()).toEqual([
-    new Point(1, 1),
-    new Point(2, 1),
+    new Point(0, 0),
+    new Point(2, 0),
     new Point(2, 2),
-    new Point(3, 2),
-    new Point(3, 1),
+    new Point(0, 1),
   ])
 })
