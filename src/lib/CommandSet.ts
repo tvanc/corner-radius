bbc34652d78eb32c835302a9202c2e5f9a0e9aee
Travis Van Couvering <travis@tvanc.com>
2021-07-22 20:59:30 -0700

    Add toString() for debugging CommandSet

--- src/lib/CommandSet.ts
+++ src/lib/CommandSet.ts
@@ -4,4 +4,10 @@ export class CommandSet {
   constructor(commands: string[][]) {
     this.commands = commands
   }
+
+  toString() {
+    return this.commands
+      .reduce((str, c) => str + c.join(" ") + " ", "")
+      .slice(0, -1)
+  }
 }
