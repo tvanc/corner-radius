5aa91e913cca37bfecd136bc1fe8026f84d67fb4
Travis Van Couvering <travis@tvanc.com>
2021-07-27 22:10:56 -0700

    WIP test for radial rounder

--- src/lib/class/PathRounder/__test__/RadialPathRounder.test.ts
+++ src/lib/class/PathRounder/__test__/RadialPathRounder.test.ts
@@ -2,15 +2,11 @@ import PolySimple from "../../../../gpc/geometry/PolySimple"
 import { Path } from "../../Path"
 import Point from "../../../../gpc/geometry/Point"
 import RadialPathRounder from "../RadialPathRounder"
+import CurveCommand from "../../Command/CubicCurve"
 
-it("Rounds given positive numbers", () => {})
-
-it("Does not round with negative numbers", () => {})
-
-it("Has no effect given a radius of zero", () => {})
-
-it("Does not exceed line lengths", () => {
+it("Rounds to given positive numbers", () => {
   const lineLength = 10
+  const radius = 15
   const rounder = new RadialPathRounder()
   const path = Path.fromPoly(
     new PolySimple([
@@ -20,5 +16,12 @@ it("Does not exceed line lengths", () => {
     ]),
   )
 
-  rounder.roundPath(path)
+  const roundedPath = rounder.roundPath(path)
+  expect(roundedPath).toBeInstanceOf(CurveCommand)
 })
+
+it("Does not round with negative numbers", () => {})
+
+it("Has no effect given a radius of zero", () => {})
+
+it("Does not exceed line lengths", () => {})
