00ffaa910f79f2ea80dd699152a5380dcbaacae8
Travis Van Couvering <travis@tvanc.com>
2021-08-29 19:35:07 -0700

    Add a getDistance() util function

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -4,3 +4,7 @@ export function getSlope(p1: Point, p2: Point): number {
   //     |--delta y--|   |--delta x--|
   return (p2.y - p1.y) / (p2.x - p1.x)
 }
+
+export function getDistance(p1: Point, p2: Point) {
+  return Math.sqrt((p2.x - p1.x) ** 2 + (p2.y - p1.y) ** 2)
+}
