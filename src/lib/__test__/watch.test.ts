3014ae0329ec4f945840c660c54d861408ab515b
Travis Van Couvering <travis@tvanc.com>
2021-09-17 20:03:57 -0700

    Add test for watching resize events

--- src/lib/__test__/watch.test.ts
+++ src/lib/__test__/watch.test.ts
@@ -7,7 +7,11 @@ import * as watchModule from "../watch"
 
 const watchElementId = "watchMe"
 
+let traceSpy
+
 beforeEach(function () {
+  traceSpy = jest.spyOn(traceModule, "trace")
+
   document.body.innerHTML = `
     <div id="${watchElementId}">
       Some Content
@@ -16,11 +20,25 @@ beforeEach(function () {
 
 it("Mutations are watched when expected", async () => {
   const el = document.getElementById(watchElementId)
-  const traceSpy = jest.spyOn(traceModule, "trace")
+
   watchModule.watch(el, { mutations: true })
   el.innerHTML = "bloop"
 
-  // Introduce tiny delay before assertions to allow mutation observer to have run
+  // Introduce tiny delay before assertions to allow mutation observer to run
+  await new Promise((r) => setTimeout(r))
+
+  expect(traceSpy).toHaveBeenCalledTimes(1)
+  expect(traceSpy).toHaveBeenCalledWith(el)
+})
+
+it("Resize observer fires on resize", async () => {
+  const el = document.getElementById(watchElementId)
+
+  el.style.height = "10vh"
+  watchModule.watch(el, { elementResize: true })
+  el.style.height = "100vh"
+
+  // Introduce tiny delay before assertions to allow mutation observer to run
   await new Promise((r) => setTimeout(r))
 
   expect(traceSpy).toHaveBeenCalledTimes(1)
