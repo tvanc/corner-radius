3bd6009f4517051394fa12275e143cb1c75ad261
Travis Van Couvering <travis@tvanc.com>
2023-11-18 00:01:47 -0800

    Simplify `roundPathFromPoints()`

--- src/lib/svg-round-corners/index.js
+++ src/lib/svg-round-corners/index.js
@@ -24,58 +24,55 @@ import Close from "../class/Command/Close"
  */
 export function roundPathFromPoints(points, r) {
   const commands = []
-  points
-    .map(
-      (el) =>
-        new Point(parseFloat(el.x.toFixed(2)), parseFloat(el.y.toFixed(2))),
+
+  for (let i = 0; i < points.length; ++i) {
+    const el = points[i]
+    const largeArcFlag = false
+    const prev = getPreviousPoint(i, points)
+    const next = getNextPoint(i, points)
+    const anglePrv = getAngle(el, prev)
+    const angleNxt = getAngle(el, next)
+    const angle = angleNxt - anglePrv // radians
+    const degrees = angle * (180 / Math.PI)
+
+    // prevent arc crossing the next command
+    const shortest = shortestLength(el, prev, next)
+    const maxRadius = Math.abs(getTangentNoHyp(angle / 2, shortest / 2))
+    const radius = Math.min(r, maxRadius)
+
+    const { offset, sweepFlag } = getOffset(angle, radius)
+
+    const prevPoint = [
+      el.x + getOppositeLength(anglePrv, offset),
+      el.y + getAdjacentLength(anglePrv, offset),
+    ]
+
+    const nextPoint = [
+      el.x + getOppositeLength(angleNxt, offset),
+      el.y + getAdjacentLength(angleNxt, offset),
+    ]
+
+    const linePoint = new Point(
+      parseFloat(prevPoint[0].toFixed(3)),
+      parseFloat(prevPoint[1].toFixed(3)),
     )
-    .map((el, i, arr) => {
-      const largeArcFlag = false
-      const prev = getPreviousPoint(i, arr)
-      const next = getNextPoint(i, arr)
-      const anglePrv = getAngle(el, prev)
-      const angleNxt = getAngle(el, next)
-      const angle = angleNxt - anglePrv // radians
-      const degrees = angle * (180 / Math.PI)
 
-      // prevent arc crossing the next command
-      const shortest = shortestLength(el, prev, next)
-      const maxRadius = Math.abs(getTangentNoHyp(angle / 2, shortest / 2))
-      const radius = Math.min(r, maxRadius)
+    const arcPoint = new Point(
+      parseFloat(nextPoint[0].toFixed(3)),
+      parseFloat(nextPoint[1].toFixed(3)),
+    )
 
-      const { offset, sweepFlag } = getOffset(angle, radius)
+    // there only need be a curve if and only if the next marker is a corner
+    if (i === 0) {
+      commands.push(new MoveTo(linePoint))
+    } else {
+      commands.push(new LineTo(linePoint))
+    }
 
-      const prevPoint = [
-        el.x + getOppositeLength(anglePrv, offset),
-        el.y + getAdjacentLength(anglePrv, offset),
-      ]
-
-      const nextPoint = [
-        el.x + getOppositeLength(angleNxt, offset),
-        el.y + getAdjacentLength(angleNxt, offset),
-      ]
-
-      const linePoint = new Point(
-        parseFloat(prevPoint[0].toFixed(3)),
-        parseFloat(prevPoint[1].toFixed(3)),
-      )
-
-      const arcPoint = new Point(
-        parseFloat(nextPoint[0].toFixed(3)),
-        parseFloat(nextPoint[1].toFixed(3)),
-      )
-
-      // there only need be a curve if and only if the next marker is a corner
-      if (i === 0) {
-        commands.push(new MoveTo(linePoint))
-      } else {
-        commands.push(new LineTo(linePoint))
-      }
-
-      commands.push(
-        new Arc(radius, radius, degrees, largeArcFlag, sweepFlag, arcPoint),
-      )
-    })
+    commands.push(
+      new Arc(radius, radius, degrees, largeArcFlag, sweepFlag, arcPoint),
+    )
+  }
 
   commands.push(new Close())
 
